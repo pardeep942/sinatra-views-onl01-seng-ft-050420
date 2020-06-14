52ba6cd3b87b4835b18d3bff0a61198cbd909439
PARDEEP KAUR <pkaur84@aol.com>
2020-06-14 18:53:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,16 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-	  "<h1> Hello World</h1>"
-	  
-		
-	end
-	
-	get '/' do
       erb :index
+    end 
+    
+    get '/info' do
+      erb :info
     end
+
+end
+
  
-    get "/info" do
-      "Testing the info page"
-    end
-  end
+  
